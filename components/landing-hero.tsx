168a0761850c067553bbac7217bcd3f88b223517
Chrismaganga <chrismaganga3@gmail.com>
2023-07-25 13:47:18 +0200

    add

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -12,7 +12,7 @@ export const LandingHero = () => {
   return (
     <div className="text-white font-bold py-36 text-center space-y-5">
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
-        <h1>The Best AI Tool for</h1>
+        <h2>The Best AI Tool for</h2>
         <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
           <TypewriterComponent
             options={{
@@ -29,11 +29,11 @@ export const LandingHero = () => {
         </div>
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
-        Create content using AI 10x faster.
+        Advanced AI 10x faster.
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
-          <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-full font-semibold">
+          <Button variant="premium" className="md:text-lg p-4 md:p-6 rounded-md font-semibold">
             Start Generating For Free
           </Button>
         </Link>
